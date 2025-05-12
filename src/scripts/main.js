ea542e8830cd430e51408181375dda1fedf0391b
Grégoire <puma-cattle-bunch@duck.com>
2025-05-12 15:14:25 +0200

    inclinaison aléatoire pour le titre

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 window.onload = () => {
-    console.log("hey");
+    const titleArray = document.querySelectorAll(".endemic-trees");
+    titleArray.forEach(el => {
+        const letterArray = el.innerHTML.split("");
+        el.innerHTML = "";
+        const possibleAngles = [-10, -5, 5, 10];
+        letterArray.forEach(letter => {
+            const r = possibleAngles[Math.round(Math.random() * (possibleAngles.length - 1))];
+            if(letter === " ") letter = "&nbsp;";
+            el.innerHTML += `<span class="endemic-trees-letter" style="transform: rotate(${r}deg);">${letter}</span>`;
+        });
+    });
 };
